a62b37df7c6db9e6b190ee76f528c94bfb1296f1
dducks <dducks@1f741dbd-56e7-e411-9769-9d61e9ac67af>
2015-04-21 13:07:45 +0000

    Added events! Started on Simulation

--- physics/Simulation.h
+++ physics/Simulation.h
@@ -11,17 +11,40 @@
 #include <physics/Particle.h>
 #include <math/Line.h>
 #include <physics/Wall.h>
+
 #include <vector>
+#include <functional>
 
 template < unsigned int DIM >
 class Simulation {
 private:
     std::vector< Particle<DIM> > particles;
     std::vector< Wall<DIM> > walls;
+    std::vector< std::function<void(Simulation&)> > funcTriggers;
     
-    Vector<DIM> cellSize;
+    double endTime;
+    double rescaleTime;
 public:
     
+    /**
+     * Constructs a Simulation object with a synchronisation / rescale event
+     * every rescaleTime seconds, which will end at endTime
+     * @param endTime the time at which this simulation will end
+     * @param rescaleTime the time at which a rescaleEvent is needed
+     */
+    Simulation(double endTime, double rescaleTime) : endTime(endTime), rescaleTime(rescaleTime) {
+        
+    }
+    
+    std::size_t registerFunction(std::function<void(Simulation&)>&& func) {
+        std::size_t idx = funcTriggers.size();
+        funcTriggers.emplace_back(func);
+        return idx;
+    }
+    
+    void run() {
+        
+    }
 };
 
 #endif	/* SIMULATION_H */
